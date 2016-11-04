28538b68a9037242847a8949ddb4bbae809d0b67
85758586 <85758586@github.com>
2016-11-04 16:41:59 +0800

    加入排序

---
+++ DataStructure/Section9/src/Main.java
@@ -0,0 +1,30 @@
+/**
+ * <h3></h3>
+ * Created by root on 2016/11/4.
+ */
+public class Main {
+
+    private static int mDatas[] = {3, 5, 0, 20, 12, -1, -7, 66, 12, 21};
+
+    public static void main(String args[]){
+        Sorter sorter = new Sorter();
+        sorter.bubbleSort1(mDatas);
+        for (int i = 0; i < mDatas.length; i++){
+            System.out.print(mDatas[i] + ",");
+        }
+        System.out.println();
+
+        sorter.bubbleSort2(mDatas);
+        for (int i = 0; i < mDatas.length; i++){
+            System.out.print(mDatas[i] + ",");
+        }
+        System.out.println();
+
+        sorter.insertSort(mDatas);
+        for (int i = 0; i < mDatas.length; i++){
+            System.out.print(mDatas[i] + ",");
+        }
+        System.out.println();
+
+    }
+}
---
+++ DataStructure/Section9/src/Sorter.java
@@ -0,0 +1,78 @@
+/**
+ * <h3></h3>
+ * Created by root on 2016/11/4.
+ */
+public class Sorter {
+
+    public void bubbleSort1(int[] datas){
+        for (int i = 0; i < datas.length - 1; i++){
+            for (int j = i + 1; j < datas.length; j++){
+                if (datas[i] > datas[j]){
+                    int temp = datas[i];
+                    datas[i] = datas[j];
+                    datas[j] = temp;
+                }
+            }
+        }
+    }
+
+    public void bubbleSort2(int[] datas){
+        for (int i = 0; i < datas.length - 1; i++){
+            for (int j = i; j < datas.length - 1; j++){
+                if(datas[j] > datas[j + 1]){
+                    int temp = datas[j];
+                    datas[j] = datas[j + 1];
+                    datas[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void insertSort(int[] datas){
+        int temps[] = new int[datas.length];
+        for (int i = datas.length - 1; i >=0; i--){
+            for (int j = 0; j < temps.length; j++){
+                if (datas[i] < temps[j]){
+                    for (int k = temps.length - 2; k > j; k--) {
+                        temps[k + 1] = temps[k];
+                    }
+                    temps[j] = datas[i];
+                }
+            }
+        }
+
+        datas = temps;
+    }
+
+    public void quickSort(int[] datas){
+
+    }
+
+    public void shellSort(int[] datas){
+
+    }
+
+    public void mergeSort(int[] datas){
+
+    }
+
+    public void heapSort(int[] datas){
+
+    }
+
+    //目前不支持负数
+    public void bucketSort(int[] datas){
+        int max = datas[0];
+        for (int i = 1; i < datas.length; i++){
+            if (max < datas[i]){
+                max = datas[i];
+            }
+        }
+
+        int temps[] = new int[max];
+
+        for (int i = 0; i < datas.length; i++){
+            temps[datas[i]] += 1;
+        }
+    }
+}
